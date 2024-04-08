2c7ac35c12bdc2ff004f78c6975825ff6777953e
Mallow <mallory.scotton@epitech.eu>
2024-04-08 18:13:30 +0200

    [FIX] Fixing L6 coding style error

--- moon/maths/distance.c
+++ moon/maths/distance.c
@@ -15,6 +15,7 @@ int distance2i(v2i pts1, v2i pts2)
 {
     int dx = pts2.x - pts1.x;
     int dy = pts2.y - pts1.y;
+
     return ((int)sqrtf((dx * dx) + (dy * dy)));
 }
 
@@ -23,6 +24,7 @@ float distance2f(v2f pts1, v2f pts2)
 {
     float dx = pts2.x - pts1.x;
     float dy = pts2.y - pts1.y;
+
     return (sqrtf((dx * dx) + (dy * dy)));
 }
 
@@ -31,6 +33,7 @@ int distance2u(v2u pts1, v2u pts2)
 {
     int dx = pts2.x - pts1.x;
     int dy = pts2.y - pts1.y;
+
     return ((int)sqrtf((dx * dx) + (dy * dy)));
 }
 
@@ -40,5 +43,6 @@ float distance3f(v3f pts1, v3f pts2)
     float dx = pts2.x - pts1.x;
     float dy = pts2.y - pts1.y;
     float dz = pts2.y - pts1.y;
+
     return (sqrtf((dx * dx) + (dy * dy) + (dz * dz)));
 }

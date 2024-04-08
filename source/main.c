f41813ec395d37d7e3665d471fc3994fadbb19db
Mallow <mallory.scotton@epitech.eu>
2024-04-08 19:12:31 +0200

    [ADD] Adding main base

--- source/main.c
+++ source/main.c
@@ -10,7 +10,28 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "moon.h"
 
+///////////////////////////////////////////////////////////////////////////////
+static void parse_events(sfEvent evt)
+{
+    if (evt.type == sfEvtClosed)
+        sfRenderWindow_close(Win.self);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
+    sfEvent evt;
+
+    init_time();
+    create_window(0, 0, WIN_WINDOWED);
+    while (sfRenderWindow_isOpen(Win.self)) {
+        update_time();
+        while (sfRenderWindow_pollEvent(Win.self, &evt))
+            parse_events(evt);
+        sfRenderWindow_clear(Win.self, sfWhite);
+        sfRenderWindow_display(Win.self);
+    }
+    destroy_window();
+    destroy_time();
     return (EXIT_SUCCESS);
 }

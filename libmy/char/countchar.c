aa40de6fc585506ba2b9bb11a6616099dc350452
Mallow <mallory.scotton@epitech.eu>
2024-04-09 23:48:16 +0200

    [REMOVE] Removing comment in the function

--- libmy/char/countchar.c
+++ libmy/char/countchar.c
@@ -11,20 +11,6 @@
 #include "my.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-/// \brief Count the occurrences of a specified character in a null-terminated
-///        string.
-///
-/// This function iterates through the input null-terminated string 'str' and
-/// increments the count for each occurrence of the specified character 'ch'.
-/// The final count is returned.
-///
-/// \param str  Pointer to the null-terminated string.
-/// \param ch   The character to count in the string.
-///
-/// \return     The number of occurrences of the specified character in the
-///             string.
-///
-///////////////////////////////////////////////////////////////////////////////
 ulong_t my_countchar(cstring_t str, char ch)
 {
     ulong_t count = ch == '\0' ? 1 : 0;

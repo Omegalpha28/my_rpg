e2b78ee5db3a57673715c489c491433415b5a824
Mallow <mallory.scotton@epitech.eu>
2024-04-09 17:46:45 +0200

    [ADD] Adding rpg related header

---
+++ include/rpg.h
@@ -0,0 +1,86 @@
+/*
+** EPITECH PROJECT, 2024
+** csfml-engine
+** File description:
+** rpg
+*/
+
+#ifndef RPG_H_
+    #define RPG_H_
+
+///////////////////////////////////////////////////////////////////////////////
+// Headers
+///////////////////////////////////////////////////////////////////////////////
+    #include "moon.h"
+    #include "my.h"
+
+///////////////////////////////////////////////////////////////////////////////
+///
+///                           _______   _______    ______
+///                          /       \ /       \  /      \.
+///                          $$$$$$$  |$$$$$$$  |/$$$$$$  |
+///                          $$ |__$$ |$$ |__$$ |$$ | _$$/
+///                          $$    $$< $$    $$/ $$ |/    |
+///                          $$$$$$$  |$$$$$$$/  $$ |$$$$ |
+///                          $$ |  $$ |$$ |      $$ \__$$ |
+///                          $$ |  $$ |$$ |      $$    $$/
+///                          $$/   $$/ $$/        $$$$$$/
+///                               an {EPITECH} project.
+///
+/// Promotion:      2028
+/// Language:       C
+/// Compilation:    Makefile
+/// Libraries:      CSFML, Maths
+/// Authors:
+///     - Mallory Scotton (@mallory-scotton)
+///     - Nathan Fievet (@natan-fievet)
+///     - Hugo Cathelain (@hugo-cathelain)
+///     - Ossan Msoili (@Omegalpha28)
+///
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Rules:
+///     - The player needs to have characteristics which you can find in the
+///       status menu.
+///     - The player can fight enemies, statistics will impact the fights
+///       results.
+///     - You need to implement at least one quest.
+///     - The player must have an inventory which can contain a limited set of
+///       items.
+///     - The player can earn experience by winning fights and accomplishing
+///       specific actions.
+///     - With enough experience, the player can level up, upgrading its
+///       statistics.
+///
+///////////////////////////////////////////////////////////////////////////////
+///
+///      AAA   KK  KK        XX    XX  OOOOO  LL       OOOOO  TTTTTTT LL
+///     AAAAA  KK KK          XX  XX  OO   OO LL      OO   OO   TTT   LL
+///    AA   AA KKKK   _____    XXXX   OO   OO LL      OO   OO   TTT   LL
+///    AAAAAAA KK KK          XX  XX  OO   OO LL      OO   OO   TTT   LL
+///    AA   AA KK  KK        XX    XX  OOOO0  LLLLLLL  OOOO0    TTT   LLLLLLL
+///                            ._ .__    _    .__
+///                            |_)|_ |V||_||/ |_
+///                            | \|__| || ||\ |__
+///
+/// Credits:    2Awesome Studio (https://www.2awesomestudio.com)
+///
+/// On a pale blue dot, somewhere in the observable universe
+/// lives an extraordinary creature known locally as the axolotl.
+/// These aquatic salamanders live a free and peaceful life
+/// and with their bellies full, and no natural predators in sight, it's
+/// easy to rest by the warmth of the campfire.
+/// However, letting your guard down can come at great cost...
+/// ...
+/// This axolotl has slept through the entire incident.
+/// With little time to think about their lost children, their sole
+/// motivation is the desire to feed.
+/// A full axolotl is a happy axolotl.
+/// O-oh dear!
+/// Axolotls can excuse stolen children, but they draw the line at
+/// stealing food.
+/// Someone is about to get f**** up.
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#endif /* !RPG_H_ */

61f6e8feb3b1f9c9a1416b703e40847a47b0d964
Mallow <mallory.scotton@epitech.eu>
2024-04-10 19:38:51 +0200

    [COMMENT] Comment the actor related functions

--- include/moon/objects.h
+++ include/moon/objects.h
@@ -54,35 +54,114 @@ extern struct pool_s {
 } Pool;
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Create a new actor base on a creature and a position
+///
+/// \param creature     The reference creature for the actor
+/// \param position     The default position of the actor
+///
+/// \return The newly created actor
+///
+///////////////////////////////////////////////////////////////////////////////
 actor_t *actor_create(creatures_t *creature, v2f_t position);
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Destroy the actor and remove it from the pool
+///
+/// \param act          The actor to destroy
+///
 ///////////////////////////////////////////////////////////////////////////////
 void actor_destroy(actor_t *act);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Move the actor base of an offset
+///
+/// \param act          The actor to move
+/// \param offset       Vector2f of the deplacement offset
+///
+///////////////////////////////////////////////////////////////////////////////
 void actor_move(actor_t *act, v2f_t offset);
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Rotate the actor base of an angle
+///
+/// \param act          The actor to rotate
+/// \param angle        Float angle to add to the actor
+///
 ///////////////////////////////////////////////////////////////////////////////
 void actor_rotate(actor_t *act, float angle);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Scale an actor base of factors
+///
+/// \param act          The actor to scale
+/// \param factors      Scaling factors applied to the actor
+///
+///////////////////////////////////////////////////////////////////////////////
 void actor_scale(actor_t *act, v2f_t factors);
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Draw the actor on the screen if need to be drawed, and with the
+/// current animation
+///
+/// \param act          The actor to draw
+///
 ///////////////////////////////////////////////////////////////////////////////
 void actor_draw(actor_t *act);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Set the actor active sheet using the index of the sheet
+///
+/// \param act          The focused actor
+/// \param id           The index of the wanted sheet
+///
+/// \return True if the sheet has been updated, false otherwise
+///
+///////////////////////////////////////////////////////////////////////////////
 bool_t actor_set_sheet_id(actor_t *act, uint_t id);
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Set the actor active sheet using the name of the sheet
+///
+/// \param act          The focused actor
+/// \param name         The name of the wanted sheet
+///
+/// \return True if the sheet has been updated, false otherwise
+///
 ///////////////////////////////////////////////////////////////////////////////
 bool_t actor_set_sheet(actor_t *act, cstring_t name);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Set the actor active animation using the index of the animation
+///
+/// \param act          The focused actor
+/// \param id           The index of the wanted animation
+///
+/// \return True if the animation has been updated, false otherwise
+///
+///////////////////////////////////////////////////////////////////////////////
 bool_t actor_set_anim_id(actor_t *act, uint_t id);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// \brief Set the actor active animation using the name of the animation
+///
+/// \param act          The focused actor
+/// \param name         The name of the wanted animation
+///
+/// \return True if the animation has been updated, false otherwise
+///
+///////////////////////////////////////////////////////////////////////////////
 bool_t actor_set_anim(actor_t *act, cstring_t name);
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Set the actor active varaint using the name or the index of the
+/// variant
+///
+/// \param act          The focused actor
+/// \param name         The name of the wanted variant
+/// \param id           The index of the wanted variant
+///
+/// \return True if the variant has been updated, false otherwise
+///
 ///////////////////////////////////////////////////////////////////////////////
 bool_t actor_set_variant(actor_t *act, cstring_t name, uint_t id);
 

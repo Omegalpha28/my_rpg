f7ff6e948edc2978b2afe80afafb88a78834aebe
Hugo <hugo.cathelain@epitech.eu>
2024-04-15 12:03:38 +0200

    [FIX] fix coding style errors

--- source/controller/camera.c
+++ source/controller/camera.c
@@ -25,11 +25,7 @@ static void draw_debug_safe(void)
     sfRectangleShape_setPosition(cam, (v2f_t){0.0f, 0.0f});
     sfRenderWindow_drawRectangleShape(Win.self, cam, NULL);
 }
-//? if player outside safe place; move camera 
+//? if player outside safe place; move camera
 //? if map ends and if player isnt in safe place move or not ?
-
-
 //? dash movement
-
-
 //? collisions in moon/object/actor ?

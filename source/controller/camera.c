c01706127601ab37368fc145762e6f8f2b7d4b87
Hugo <hugo.cathelain@epitech.eu>
2024-04-15 12:02:28 +0200

    [FIX] move the debug safe place function to the camera file

--- source/controller/camera.c
+++ source/controller/camera.c
@@ -9,3 +9,27 @@
 // Headers
 ///////////////////////////////////////////////////////////////////////////////
 #include "rpg.h"
+
+///////////////////////////////////////////////////////////////////////////////
+static void draw_debug_safe(void)
+{
+    sfRectangleShape *cam = sfRectangleShape_create();
+
+    sfRectangleShape_setSize(cam, (v2f_t){Win.viewWidth - 100.0f,
+        Win.viewHeight - 100.0f});
+    sfRectangleShape_setOutlineColor(cam, sfBlue);
+    sfRectangleShape_setOutlineThickness(cam, 1.0f);
+    sfRectangleShape_setFillColor(cam, sfTransparent);
+    sfRectangleShape_setOrigin(cam, (v2f_t){(Win.viewWidth - 100.0f) / 2,
+        (Win.viewHeight - 100.0f) / 2});
+    sfRectangleShape_setPosition(cam, (v2f_t){0.0f, 0.0f});
+    sfRenderWindow_drawRectangleShape(Win.self, cam, NULL);
+}
+//? if player outside safe place; move camera 
+//? if map ends and if player isnt in safe place move or not ?
+
+
+//? dash movement
+
+
+//? collisions in moon/object/actor ?
--- source/game/loop.c
+++ source/game/loop.c
@@ -27,22 +27,6 @@ static void draw_debug_line(actor_t *act, v2f_t cr)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-static void draw_debug_safe(void)
-{
-    sfRectangleShape *cam = sfRectangleShape_create();
-
-    sfRectangleShape_setSize(cam, (v2f_t){Win.viewWidth - 100.0f,
-        Win.viewHeight - 100.0f});
-    sfRectangleShape_setOutlineColor(cam, sfBlue);
-    sfRectangleShape_setOutlineThickness(cam, 1.0f);
-    sfRectangleShape_setFillColor(cam, sfTransparent);
-    sfRectangleShape_setOrigin(cam, (v2f_t){(Win.viewWidth - 100.0f) / 2,
-        (Win.viewHeight - 100.0f) / 2});
-    sfRectangleShape_setPosition(cam, (v2f_t){0.0f, 0.0f});
-    sfRenderWindow_drawRectangleShape(Win.self, cam, NULL);
-}
-
-///////////////////////////////////////////////////////////////////////////////
 void game_loop(void)
 {
     actor_t *act = Player.ref;

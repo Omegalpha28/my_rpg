445f4a39d1124596c436723060c2da8ecae08a3c
nathan <nathan.fievet@epitech.eu>
2024-04-17 16:27:53 +0200

    [UPDATE] angry walk is on point

--- source/entity/melee.c
+++ source/entity/melee.c
@@ -11,22 +11,27 @@
 #include "rpg.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-void melee(entity_t *evil)
+void caterkillah_ai(entity_t *evil)
 {
     v2f_t move = movetowards2f(evil->actor->position,
-        endpoint2f(Player.ref->position, evil->actor->position, evil->radius),
-        (evil->speed * Time.deltaTime) / 25);
-    v2f_t velocity = subtract2f(move, evil->actor->position);
+        endpoint2f(Player.ref->position, evil->actor->position,
+            evil->radius / 4), (evil->speed * Time.deltaTime) / 25);
     float_t curr_rad = distance2f(evil->actor->position, Player.ref->position);
 
     evil->actor->scale.x = move.x - evil->actor->position.x > 0 ? 1.0f : -1.0f;
-    if (curr_rad >= evil->radius){
+    if (curr_rad >= (evil->radius / 4))
         evil->actor->position = move;
-    } else {
+    else
         evil->actor->scale.x *= -1;
-        velocity = (v2f_t){0.0f, 0.0f};
-    }
-    actor_set_anim(evil->actor, velocity.x != 0.0f || velocity.y != 0.0f ?
-        "walk" : "idle");
+    actor_set_anim(evil->actor, curr_rad >= evil->radius ?
+        "walk" : "angrywalk");
     actor_draw(evil->actor);
 }
+
+/*
+its more of a case of idle to attack. once it "attacks" it goes into angrywalk
+where it will "chase" the player (chase in parenthesis cause they can just
+straight up miss.)
+
+
+*/

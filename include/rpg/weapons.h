cdc3aa88eae63b8030e14d9705c0ea3b47eb26e8
nathan <nathan.fievet@epitech.eu>
2024-04-22 11:39:46 +0200

    [FIX] removing frowned upon function and sepereation/organization of weapons

---
+++ include/rpg/weapons.h
@@ -0,0 +1,83 @@
+/*
+** EPITECH PROJECT, 2024
+** my_rpg
+** File description:
+** weapons
+*/
+
+#ifndef WEAPONS_H_
+    #define WEAPONS_H_
+
+///////////////////////////////////////////////////////////////////////////////
+// Headers
+///////////////////////////////////////////////////////////////////////////////
+    #include "rpg.h"
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef enum weapon_type_e {
+    weaponLaser,
+    weaponBullet,
+    weaponMelee,
+    weaponExplosive,
+    WEAPON_TYPE_COUNT
+} weapon_type_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param sender
+/// \param origin
+/// \param destination
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct bullet_s {
+    uint_t sender;
+    sfSprite *sprite;
+    v2f_t origin;
+    v2f_t destination;
+    v2f_t position;
+    uint_t destroyed;
+    uint_t rect_sprite;
+    uint_t num_sheet;
+    uint_t size_max_x;
+    uint_t begin;
+    sfCircleShape *area;
+} bullet_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param type
+/// \param ammos
+/// \param damage
+/// \param firerate
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct weapon_s {
+    weapon_type_t type;
+    uint_t ammos;
+    uint_t damage;
+    float firerate;
+} weapon_t;
+
+extern struct bullet_list_s {
+    bullet_t **array;
+    uint_t count;
+} Bullet_List;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param evt
+///
+///////////////////////////////////////////////////////////////////////////////
+void game_loop(void);
+bullet_t *bullet_creation(uint_t sender, uint_t size_rect, uint_t spritesheet,
+    uint_t size_max);
+void draw_bullets(void);
+bool_t destroy_bullet(bullet_t *bullet);
+
+#endif /* !WEAPONS_H_ */

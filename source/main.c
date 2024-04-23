40b3caeab9143b3994515360d6797c7ba9d54512
Mallow <mallory.scotton@epitech.eu>
2024-04-23 19:40:55 +0200

    [ADD] Adding flags to the game for editor and debug mode.

--- source/main.c
+++ source/main.c
@@ -11,12 +11,27 @@
 #include "rpg.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-int main(void)
+static void check_program_flags(int argc, warray_t argv)
+{
+    if (argc == 1)
+        return;
+    if (WCMP(argv, "--editor") || WCMP(argv, "-e")) {
+        load_zone("biome1");
+        Editor.zone = Assets.zones[0];
+        Engine.scene = SCENE_LEVEL_EDITOR;
+    }
+    if (WCMP(argv, "--debug") || WCMP(argv, "-d"))
+        Engine.debugMode = true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, warray_t argv)
 {
     srand((unsigned)time(NULL));
     if (!init_time() || !init_assets() || !create_window(0, 0, WIN_WINDOWED))
         return (EXIT_FAILURE);
     Player.ref = actor_create(Assets.creatures[CREATURE_PLAYER], V2F1(0.0f));
+    check_program_flags(argc, argv);
     while (sfRenderWindow_isOpen(Win.self)) {
         update_time();
         sfRenderWindow_clear(Win.self, EDITOR_BACKGROUND);

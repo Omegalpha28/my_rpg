70436c6d86708b42eee58d3f9225cdc7ace793c6
Mallow <mallory.scotton@epitech.eu>
2024-04-30 14:17:52 +0200

    [ADD] Adding a new input about collision

--- source/editor/interfaces/init.c
+++ source/editor/interfaces/init.c
@@ -10,6 +10,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "rpg.h"
 
+///////////////////////////////////////////////////////////////////////////////
 static void handle_coord_input(input_t *input)
 {
     float value = 0.0f;
@@ -30,6 +31,13 @@ static void handle_coord_input(input_t *input)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+static void handle_checkbox_input(input_t *input)
+{
+    if (input == Editor.inputs[EDITOR_INPUT_COLLISION] && Editor.focus)
+        Editor.focus->collision = input->checked;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 void editor_settings_init_input(v2f_t pos)
 {
     create_input(INPUT_INTEGER, add2f(pos, V2F(40.0f, 140.0f)),
@@ -38,4 +46,7 @@ void editor_settings_init_input(v2f_t pos)
     create_input(INPUT_INTEGER, add2f(pos, V2F(40.0f, 192.0f)),
         V2F(200.0f, 42.0f), "Coordinate Y");
     Editor.inputs[EDITOR_INPUT_Y]->onInput = &handle_coord_input;
+    create_input(INPUT_CHECKBOX, add2f(pos, V2F(160.0f, 244.0f)),
+        V2F(42.0f, 42.0f), NULL);
+    Editor.inputs[EDITOR_INPUT_COLLISION]->onInput = &handle_checkbox_input;
 }

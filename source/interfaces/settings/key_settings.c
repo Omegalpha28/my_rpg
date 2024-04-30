9ba63636a173294e0ae00fc9f4e8dccc2779ebff
Hugo <hugo.cathelain@epitech.eu>
2024-04-30 20:26:53 +0200

    [FIX] better spelling of french sentence in a button

--- source/interfaces/settings/key_settings.c
+++ source/interfaces/settings/key_settings.c
@@ -14,7 +14,7 @@ void draw_keybinds(void)
 {
     float scal = 4.0f * 16.0f * Win.height / Win.viewHeight * 0.35f;
 
-    draw_text("Invert clicks",
+    draw_text("Invert click",
         PX_TO_MAPF(V2F(Win.width / 4, Win.height / 2 - scal * 3)),
         0.35F, on_off(V2F(Win.width / 4, Win.height / 2 - scal * 3), &INVERT));
     draw_text("Sensitivity",

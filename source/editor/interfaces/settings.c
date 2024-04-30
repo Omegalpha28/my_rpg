2a4bdb6f06c70fdbd6312140183c01f132108ba5
Mallow <mallory.scotton@epitech.eu>
2024-04-30 15:40:37 +0200

    [ADD] Adding new flip checbox

--- source/editor/interfaces/settings.c
+++ source/editor/interfaces/settings.c
@@ -43,10 +43,56 @@ static void draw_focused_prop(v2f_t pos)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+static void find_id_and_max(uint_t *idx, uint_t *max)
+{
+    for (uint_t i = 0; i < Editor.fCount; i++) {
+        if (Editor.fProps[i] == Editor.focus) {
+            *idx = i;
+            *max = Editor.fCount;
+            return;
+        }
+    }
+    for (uint_t i = 0; i < Editor.bCount; i++) {
+        if (Editor.bProps[i] == Editor.focus) {
+            *idx = i;
+            *max = Editor.bCount;
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+static void draw_editor_settings_info(v2f_t pos, float fact)
+{
+    char zIndex[32];
+    uint_t idx = 0;
+    uint_t max = 0;
+
+    draw_text("Index", PX_TO_MAPF(add2f(pos, V2F(10.0f, 136.0f))), fact,
+        WHITE);
+    if (!Editor.focus)
+        return;
+    draw_focused_prop(add2f(pos, V2F(75.0f, 0.0f)));
+    find_id_and_max(&idx, &max);
+    snprintf(zIndex, 32, "%u - %u", idx + 1, max);
+    draw_text(zIndex, PX_TO_MAPF(add2f(pos, V2F(90.0f, 136.0f))), fact,
+        RGB(200, 200, 0));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+static void draw_editor_settings_label(v2f_t pos, float fact)
+{
+    draw_text("X", PX_TO_MAPF(add2f(pos, V2F(10.0f, 181.0f))), fact, WHITE);
+    draw_text("Y", PX_TO_MAPF(add2f(pos, V2F(10.0f, 233.0f))), fact, WHITE);
+    draw_text("Collision", PX_TO_MAPF(add2f(pos, V2F(10.0f, 285.0f))), fact,
+        WHITE);
+    draw_text("Flip", PX_TO_MAPF(add2f(pos, V2F(10.0f, 337.0f))), fact, WHITE);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 void draw_editor_settings(void)
 {
-    float fact = FACTORS(V2F1(20.0f)).x;
     v2f_t pos = {Win.width - EDITOR_PANEL_W, EDITOR_PANEL_H * 2.0f};
+    float fact = FACTORS(V2F1(20.0f)).x;
 
     if (Editor.inputCount == 0)
         editor_settings_init_input(pos);
@@ -54,11 +100,6 @@ void draw_editor_settings(void)
     draw_rect(V2F(EDITOR_PANEL_W, Win.height - EDITOR_PANEL_H * 2), pos,
         EDITOR_WIDGET);
     draw_rect(V2F1(100.0f), add2f(pos, V2F(75.0f, 0.0f)), EDITOR_BUTTON);
-    if (Editor.focus)
-        draw_focused_prop(add2f(pos, V2F(75.0f, 0.0f)));
-    draw_text("X", PX_TO_MAPF(add2f(pos, V2F(10.0f, 151.0f))), fact, WHITE);
-    draw_text("Y", PX_TO_MAPF(add2f(pos, V2F(10.0f, 203.0f))), fact, WHITE);
-    draw_text("Collision", PX_TO_MAPF(add2f(pos, V2F(10.0f, 255.0f))), fact,
-        WHITE);
-    draw_text("Flip", PX_TO_MAPF(add2f(pos, V2F(10.0f, 307.0f))), fact, WHITE);
+    draw_editor_settings_info(pos, fact);
+    draw_editor_settings_label(pos, fact);
 }

dc827d38a922eca199fff07d1d4c4a43fb21a8d9
Mallow <mallory.scotton@epitech.eu>
2024-05-02 16:56:01 +0200

    [FIX] Disable inputs and events when popups are openened

--- source/editor/events.c
+++ source/editor/events.c
@@ -15,17 +15,21 @@ void handle_editor_events(sfEvent evt)
 {
     if (evt.type == sfEvtClosed)
         sfRenderWindow_close(Win.self);
-    if (evt.type == sfEvtMouseButtonPressed)
-        handle_editor_mouse_button(evt.mouseButton);
+    if (evt.type == sfEvtKeyPressed && Editor.inputFocused &&
+        Editor.inputFocused->type != INPUT_CHECKBOX)
+        handle_editor_key_input(evt.key);
     if (evt.type == sfEvtMouseButtonReleased)
         handle_editor_mouse_released();
+    if (evt.type == sfEvtMouseButtonPressed)
+        Editor.inputFocused = NULL;
+    if (Editor.popupOpen)
+        return;
+    if (evt.type == sfEvtMouseButtonPressed)
+        handle_editor_mouse_button(evt.mouseButton);
     if (evt.type == sfEvtMouseWheelScrolled)
         handle_editor_mouse_scroll(evt.mouseWheelScroll);
     if (evt.type == sfEvtKeyPressed && !Editor.inputFocused)
         handle_editor_key_pressed(evt.key);
-    if (evt.type == sfEvtKeyPressed && Editor.inputFocused &&
-        Editor.inputFocused->type != INPUT_CHECKBOX)
-        handle_editor_key_input(evt.key);
     if (evt.type == sfEvtMouseMoved)
         handle_mouse_move();
 }

c64fb46ff493975967a6176597655f8745fc2b0c
Mallow <mallory.scotton@epitech.eu>
2024-05-06 15:35:51 +0200

    [ADD] Adding handler for speed

--- source/controller/animation.c
+++ source/controller/animation.c
@@ -29,9 +29,8 @@ void player_movement(void)
     if (RIGHT && !DANCE && !DASH)
         Player.velocity.x += SPEED;
     get_last_input();
-    Player.velocity = normalize2f(Player.velocity);
-    Player.velocity.x *= Time.deltaTime / 15;
-    Player.velocity.y *= Time.deltaTime / 15;
+    Player.velocity = multiply2f(normalize2f(Player.velocity),
+        V2F1(Time.deltaTime / 15));
     if (!DASH && !DANCE)
         actor_set_anim(Player.ref, (Player.velocity.x != 0.0f ||
             Player.velocity.y != 0.0f) ? "walk" : "idle");
@@ -55,9 +54,8 @@ static void dash_animation(void)
             Player.velocity.x += -SPEED;
         if ((RIGHT && velocity.x > 0.0f) || velocity.x > 0.0f)
             Player.velocity.x += SPEED;
-        Player.velocity = normalize2f(Player.velocity);
-        Player.velocity.x *= Time.deltaTime / 12;
-        Player.velocity.y *= Time.deltaTime / 12;
+        Player.velocity = multiply2f(normalize2f(Player.velocity),
+            V2F1(Time.deltaTime / 12));
         actor_move(Player.ref, Player.velocity);
         Player.ref->scale.x = Player.velocity.x < 0.0f ? -1.0f : 1.0f;
         camera_move();
@@ -77,6 +75,6 @@ void dash_movement(void)
             Player.velocity.y != 0.0f) ? "walk" : "idle");
         DASH = false;
     }
-    actor_move(Player.ref, Player.velocity);
+    actor_move(Player.ref, multiply2f(Player.velocity, V2F1(Player.speed)));
     camera_move();
 }

03f4f2695891f426e4b7adf9c8ad3c509d48e048
Mallow <mallory.scotton@epitech.eu>
2024-05-16 16:54:32 +0200

    [FIX] Fixing game layering

--- source/game/layering.c
+++ source/game/layering.c
@@ -125,7 +125,7 @@ static void draw_hud(void)
 {
     v2f_t cr = PX_TO_MAPF(sfMouse_getPositionRenderWindow(Win.self));
 
-    if (Engine.debugMode)
+    if (Engine.debugMode && !DASH && !DANCE)
         draw_debug_line(cr);
     draw_visor();
 }

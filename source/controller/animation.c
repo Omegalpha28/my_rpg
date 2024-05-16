2d70ef99ddc31f890572a3b7a9f9f44f5c9513c3
Mallow <mallory.scotton@epitech.eu>
2024-05-16 17:30:29 +0200

    [FIX] Fixing the dash to be a dash

--- source/controller/animation.c
+++ source/controller/animation.c
@@ -55,7 +55,8 @@ static void dash_animation(void)
         if ((RIGHT && velocity.x > 0.0f) || velocity.x > 0.0f)
             Player.velocity.x += SPEED;
         Player.velocity = multiply2f(normalize2f(Player.velocity),
-            V2F1(Time.deltaTime / 15));
+            V2F1(Time.deltaTime / (10 + (8 * (clamp(Player.ref->frame - 26, 0,
+                99))))));
         actor_move(Player.ref, Player.velocity);
         Player.ref->scale.x = Player.velocity.x < 0.0f ? -1.0f : 1.0f;
         camera_move();

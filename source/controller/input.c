70b1487dd92033a369d64c4174bcbe428267aafa
ossanomegalpha <ossan.msoili@epitech.eu>
2024-05-21 12:59:32 +0200

    [UPDATE] adding check_weapon_ammo

--- source/controller/input.c
+++ source/controller/input.c
@@ -16,8 +16,11 @@ void parse_mouse_input(sfMouseButtonEvent evt)
     bool_t pressed = (evt.type == sfEvtMouseButtonPressed);
     v2f_t cr = PX_TO_MAPF(sfMouse_getPositionRenderWindow(Win.self));
 
-    if (evt.button == Setting.shoot && pressed && !DANCE && !DASH)
+    if (check_weapon_ammo(Player.weapon) == 1)
+        return;
+    if (evt.button == Setting.shoot && pressed && !DANCE && !DASH) {
         create_bullet(Player.ref, cr, Player.weapon);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////

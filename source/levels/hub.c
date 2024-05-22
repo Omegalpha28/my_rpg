cffb6cd94e50e9b39c3310eb5cec0ef1f8667c0e
Mallow <mallory.scotton@epitech.eu>
2024-05-22 11:54:33 +0200

    [ADD/FIX/UPDATE] Adding multitude of thing for levels

--- source/levels/hub.c
+++ source/levels/hub.c
@@ -13,9 +13,17 @@
 ///////////////////////////////////////////////////////////////////////////////
 bool_t level_hub(void)
 {
-    if (Engine.level != 0 || !level_load("shared/ub"))
+    sfMusic *music = find_music("by-the-campfire");
+
+    if (Engine.level != 0 || !level_load("shared/hub"))
         return (false);
-    effect("firethrower", V2F(-91.0f, -439.0f), false);
-    effect("firethrower", V2F(91.0f, -439.0f), false);
+    end_music();
+    for (uint_t i = 0; i < Pool.propCount; i++)
+        if (CMP(Pool.props[i]->self->name, "torch"))
+            effect("firethrower", add2f(Pool.props[i]->position, V2F(0.0f,
+                -11.0f)), false);
+    sfMusic_setVolume(music, clampf(Setting.master *
+        (Setting.music / 100.0f) * 0.75f, 0.0f, 100.0f));
+    sfMusic_play(music);
     return (true);
 }

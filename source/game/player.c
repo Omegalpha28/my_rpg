968ac56e924a0a0ad527979f3cad3330a9145629
Mallow <mallory.scotton@epitech.eu>
2024-05-28 11:44:16 +0200

    [ADD] Adding purse to the player

--- source/game/player.c
+++ source/game/player.c
@@ -15,5 +15,7 @@ struct player_s Player = {
     NULL, {0.0f, 0.0f}, {0.0f, -SPEED},
     {0.0f, 0.0f}, 0, 0.0f, 0, false, {0.0f, 0.0f},
     WEAPON_RIFLE_AK, {WEAPON_RIFLE_AK, WEAPON_NO},
-    {35, 0}, 23, 1.5f, 0, 0, 0U, 0U, 0U, false, 0LU, false, false
+    {35, 0}, 23, 1.5f, 0, 0, false, 0LU, false, false, {
+        0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U
+    }
 };

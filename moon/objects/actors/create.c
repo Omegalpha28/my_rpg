d1357142fa2f3292dba70a67428437f903ab3024
Mallow <mallory.scotton@epitech.eu>
2024-05-28 11:43:08 +0200

    [FIX] Fixing coding style error

--- moon/objects/actors/create.c
+++ moon/objects/actors/create.c
@@ -11,36 +11,36 @@
 #include "moon.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-static void actor_relay(actor_t *act)
+static void actor_set_default(actor_t *act)
 {
     act->shield = 0;
     act->charges = 0;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-static void actor_init(actor_t *act, creature_t *creature, v2f_t position)
-{
-    act->animId = 0;
-    act->variantId = 0;
-    act->sheetId = 0;
-    act->frame = 0;
-    act->isVariant = false;
-    act->time = Time.currentTime;
-    act->position = position;
-    act->rotation = 0.0f;
-    act->scale = (v2f_t){1.0f, 1.0f};
-    act->sprite = sfSprite_create();
-    act->self = creature;
-    sfSprite_setPosition(act->sprite, MAP_TO_PXF(position));
-    act->draw = true;
-    act->done = false;
-    act->old_pos = position;
     act->health = 100;
     act->shield_health = 0;
     act->invincible = false;
     act->damaged = false;
     act->dead = false;
-    actor_relay(act);
+    act->draw = true;
+    act->done = false;
+    act->animId = 0;
+    act->variantId = 0;
+    act->sheetId = 0;
+    act->frame = 0;
+    act->rotation = 0.0f;
+    act->isVariant = false;
+    act->scale = V2F1(1.0f);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+static void actor_init(actor_t *act, creature_t *creature, v2f_t position)
+{
+    actor_set_default(act);
+    act->time = Time.currentTime;
+    act->position = position;
+    act->sprite = sfSprite_create();
+    act->self = creature;
+    sfSprite_setPosition(act->sprite, MAP_TO_PXF(position));
+    act->old_pos = position;
 }
 
 ///////////////////////////////////////////////////////////////////////////////

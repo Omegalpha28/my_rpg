6428688b5de1ea87502fa1147b897ac747ebd0f2
Mallow <mallory.scotton@epitech.eu>
2024-05-30 12:11:39 +0200

    [ADD] ADding music to biome1

--- source/levels/biome1.c
+++ source/levels/biome1.c
@@ -13,6 +13,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 static const uint_t LEVEL_COUNT = 6;
 
+///////////////////////////////////////////////////////////////////////////////
 static const cstring_t LEVELS[] = {
     "tutorial/02",
     "tutorial/03",
@@ -36,12 +37,25 @@ static void init_crab_boss_room(void)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+static void set_biome1_music(void)
+{
+    sfMusic *music = find_music("i-m-hungry");
+
+    end_music();
+    sfMusic_setVolume(music, clampf(Setting.master *
+        (Setting.music / 100.0f) * 0.1f, 0.0f, 100.0f));
+    sfMusic_play(music);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 bool_t level_biome1(void)
 {
     if (Engine.level < 10 || Engine.level > 19)
         return (false);
     level_load(Engine.level != 19 ? LEVELS[rand() % LEVEL_COUNT] :
         "boss/crab");
+    if (Engine.level == 10)
+        set_biome1_music();
     if (Engine.level != 19)
         use_spawner((uint_t)(clamp(Engine.level - 9, 1, 5)));
     else
